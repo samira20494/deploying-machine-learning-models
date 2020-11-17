0c742b0e3b3db62acd70dc8f17d7ff5fc2b2c761
Samira Alizadehzoj <samira.alizadehzoj@gmail.com>
2020-11-17 15:19:35 -0500

    update db script

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
+
+#2. unzip the downloaded folder
+unzip  packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+
+#3. pick only the interested files and put them in the package dataset
+mv train.csv  packages/regression_model/regression_model/datasets/
+mv test.csv  packages/regression_model/regression_model/datasets/
