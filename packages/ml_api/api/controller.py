635aa83bb6bf9712c6da530bff68c2cde4f1f86a
Samira Alizadehzoj <samira.alizadehzoj@gmail.com>
2021-02-22 21:47:43 -0500

    section-7:setup config and logging

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, request
+from api.config import get_logger
+
+_logger = get_logger(logger_name=__name__)
 
 prediction_app = Blueprint('prediction_app', __name__)
 
@@ -6,4 +9,5 @@ prediction_app = Blueprint('prediction_app', __name__)
 @prediction_app.route('/health', methods=['GET'])
 def health():
     if request.method == 'GET':
+
         return 'hello!'
