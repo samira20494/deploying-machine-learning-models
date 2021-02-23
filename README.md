a9994480d17aa1f64d5a9a905677e8d43cd97ae2
Samira Alizadehzoj <samira.alizadehzoj@gmail.com>
2021-02-23 18:41:59 -0500

    section-7: adding the prediction and version endpoints

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Deploying Machine Learning Models
 For the documentation, visit the course on Udemy.
+
+# resources for Flask:
+Flask config docs: http://flask.pocoo.org/docs/1.0/config/
+Flask application context docs: http://flask.pocoo.org/docs/1.0/appcontext/
+Pytest fixtures: https://docs.pytest.org/en/latest/fixture.html
+Pytest conftest files: https://docs.pytest.org/en/2.7.3/plugins.html?highlight=re
+
+Flask testing docs: http://flask.pocoo.org/docs/1.0/testing/
+Flask blueprints: http://flask.pocoo.org/docs/1.0/blueprints/
+
+Marshmallow: https://marshmallow.readthedocs.io/en/2.x-line/ (note that we use the 2.x line in our application, not the latest 3.x line).
+
+
--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from regression_model.predict import make_prediction
+from regression_model import __version__ as _version
+from api import __version__ as api_version
+
 from api.config import get_logger
 
 _logger = get_logger(logger_name=__name__)
@@ -9,5 +13,27 @@ prediction_app = Blueprint('prediction_app', __name__)
 @prediction_app.route('/health', methods=['GET'])
 def health():
     if request.method == 'GET':
-
         return 'hello!'
+
+
+@prediction_app.route('/version', methods=['GET'])
+def version():
+    if request.method == 'GET':
+        return jsonify({'model_version': _version,
+                        'api_version': api_version})
+
+
+@prediction_app.route('/v1/predict/regression', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        _logger.info(f'Inputs: {json_data}')
+
+        result = make_prediction(input_data=json_data)
+        _logger.info(f'Outputs: {result}')
+
+        predictions = result.get('predictions')[0]
+        version = result.get('version')
+
+        return jsonify({'predictions': predictions,
+                        'version': version})
